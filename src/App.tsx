107f764a4b0149089dd0b3275c98e2a08f5d5d7f
mramirid <amir.muh.hakim@gmail.com>
2022-08-20 17:27:26 +0700

    feat: create `<Button />` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import Button from "./components/Button";
 
 export default function App() {
-  return <Button />;
+  return (
+    <Button isLink isExternal href="https://github.com/mramirid" isDisabled>
+      Hello World
+    </Button>
+  );
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,143 @@
-export default function Button() {
-  return <button className="btn btn-primary">Some Button</button>;
+import type { CSSProperties } from "react";
+import { Link as ReactRouterLink } from "react-router-dom";
+
+type StyleProps = {
+  size?: "small" | "large";
+  style?: CSSProperties;
+  className?: string;
+  isPrimary?: boolean;
+  isDisabled?: boolean;
+  isBlock?: boolean;
+  hasShadow?: boolean;
+};
+
+type BaseProps = StyleProps & {
+  isLoading?: boolean;
+  children: string;
+  onClick?: () => void;
+};
+
+type NormalButtonProps = BaseProps & {
+  isLink?: false;
+};
+
+type BaseLinkProps = BaseProps & {
+  isLink: true;
+  href: string;
+};
+
+type InternalLinkProps = BaseLinkProps & {
+  isExternal?: false;
+};
+
+type ExternalLinkProps = BaseLinkProps & {
+  isExternal: true;
+  isBlank?: boolean;
+};
+
+type LinkProps = InternalLinkProps | ExternalLinkProps;
+
+export default function Button(props: NormalButtonProps | LinkProps) {
+  const className = buildClassName(props);
+
+  const handleClick = () => {
+    props.onClick?.();
+  };
+
+  if (props.isLoading) {
+    return <LoadingButton />;
+  }
+  if (props.isLink) {
+    return <Link {...props} className={className} onClick={handleClick} />;
+  }
+  return (
+    <NormalButton {...props} className={className} onClick={handleClick} />
+  );
+}
+
+function buildClassName(styleProps: StyleProps) {
+  const classNameSet = new Set(["btn", styleProps.className]);
+
+  if (styleProps.isPrimary) {
+    classNameSet.add("btn-primary");
+  }
+
+  switch (styleProps.size) {
+    case "small":
+      classNameSet.add("btn-sm");
+      break;
+    case "large":
+      classNameSet.add("btn-lg");
+      break;
+  }
+
+  if (styleProps.isBlock) {
+    classNameSet.add("btn-block");
+  }
+
+  if (styleProps.hasShadow) {
+    classNameSet.add("btn-shadow");
+  }
+
+  if (styleProps.isDisabled) {
+    classNameSet.add("disabled");
+  }
+
+  return Array.from(classNameSet).join(" ");
+}
+
+function LoadingButton(props: StyleProps) {
+  return (
+    <span className={props.className} style={props.style}>
+      <span className="spinner-border spinner-border-sm mx-5" />
+      <span className="sr-only">Loading...</span>
+    </span>
+  );
+}
+
+function Link(props: LinkProps) {
+  if (props.isExternal) {
+    return <ExternalLink {...props} />;
+  }
+
+  return (
+    <ReactRouterLink
+      to={props.href}
+      className={props.className}
+      style={props.style}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </ReactRouterLink>
+  );
+}
+
+function ExternalLink(props: ExternalLinkProps) {
+  const target = props.isBlank ? "_blank" : undefined;
+  const rel = props.isBlank ? "noopener noreferrer" : undefined;
+
+  return (
+    <a
+      href={props.href}
+      className={props.className}
+      style={props.style}
+      target={target}
+      rel={rel}
+    >
+      {props.children}
+    </a>
+  );
+}
+
+function NormalButton(props: NormalButtonProps) {
+  return (
+    <button
+      className={props.className}
+      style={props.style}
+      onClick={props.onClick}
+      disabled={props.isDisabled}
+    >
+      {props.children}
+    </button>
+  );
 }
