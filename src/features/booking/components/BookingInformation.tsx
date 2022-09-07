e04f5112f6beb4556d2ae96df8ff20ebda424831
mramirid <amir.muh.hakim@gmail.com>
2022-09-07 10:21:15 +0700

    fix(client): centering the content

--- src/features/booking/components/BookingInformation.tsx
+++ src/features/booking/components/BookingInformation.tsx
@@ -18,15 +18,16 @@ type BookingInformationProps = {
 
 export default function BookingInformation(props: BookingInformationProps) {
   return (
-    <Fade className={clx("flex justify-center gap-x-20", props.className)}>
+    <Fade
+      className={clx(
+        "grid grid-cols-[26.25rem_min-content_26.25rem] gap-x-20 justify-center",
+        props.className
+      )}
+    >
       <>
-        <Fade delay={300} className="py-9">
-          <PropertyDetails />
-        </Fade>
+        <PropertyDetails />
         <div className="divider divider-horizontal w-fit before:w-1px after:w-1px m-0" />
-        <Fade delay={600} className="py-9 w-80">
-          <Form />
-        </Fade>
+        <Form />
       </>
     </Fade>
   );
@@ -34,31 +35,33 @@ export default function BookingInformation(props: BookingInformationProps) {
 
 function PropertyDetails() {
   return (
-    <figure className="img-wrapper">
-      <img
-        src={property.imageUrls[0].url}
-        alt={property.name}
-        style={{ height: "16.875rem", aspectRatio: "14 / 9" }}
-      />
-      <figcaption className="mt-4 flex justify-between items-center">
-        <div>
-          <h5 className="text-xl text-secondary">{property.name}</h5>
-          <span className="text-light">
-            {property.city}, {property.country}
-          </span>
-        </div>
-        <div className="leading-7">
-          <b className="text-semibold">
-            ${CHECKOUT.duration * property.price} USD
-          </b>
-          <span className="text-light"> per </span>
-          <b className="text-semibold">
-            {CHECKOUT.duration} {property.unit}
-            {CHECKOUT.duration > 1 ? "s" : ""}
-          </b>
-        </div>
-      </figcaption>
-    </figure>
+    <Fade delay={300} className="py-9">
+      <figure className="img-wrapper">
+        <img
+          src={property.imageUrls[0].url}
+          alt={property.name}
+          style={{ height: "16.875rem" }}
+        />
+        <figcaption className="mt-4 flex justify-between items-center">
+          <div>
+            <h5 className="text-xl text-secondary">{property.name}</h5>
+            <span className="text-light">
+              {property.city}, {property.country}
+            </span>
+          </div>
+          <div className="leading-7">
+            <b className="text-semibold">
+              ${CHECKOUT.duration * property.price} USD
+            </b>
+            <span className="text-light"> per </span>
+            <b className="text-semibold">
+              {CHECKOUT.duration} {property.unit}
+              {CHECKOUT.duration > 1 ? "s" : ""}
+            </b>
+          </div>
+        </figcaption>
+      </figure>
+    </Fade>
   );
 }
 
@@ -85,53 +88,55 @@ function Form() {
   };
 
   return (
-    <form className="flex flex-col gap-y-5" onSubmit={handleSubmit(submit)}>
-      <div>
-        <label htmlFor="first-name">First Name</label>
-        <InputText
-          id="first-name"
-          type="text"
-          containerClass="mt-2"
-          {...register("firstName")}
-          errorMessage={formState.errors.firstName?.message}
-        />
-      </div>
+    <Fade delay={600} className="py-9 max-w-xs">
+      <form className="flex flex-col gap-y-5" onSubmit={handleSubmit(submit)}>
+        <div>
+          <label htmlFor="first-name">First Name</label>
+          <InputText
+            id="first-name"
+            type="text"
+            containerClass="mt-2"
+            {...register("firstName")}
+            errorMessage={formState.errors.firstName?.message}
+          />
+        </div>
 
-      <div>
-        <label htmlFor="last-name">Last Name</label>
-        <InputText
-          id="last-name"
-          type="text"
-          containerClass="mt-2"
-          {...register("lastName")}
-          errorMessage={formState.errors.lastName?.message}
-        />
-      </div>
+        <div>
+          <label htmlFor="last-name">Last Name</label>
+          <InputText
+            id="last-name"
+            type="text"
+            containerClass="mt-2"
+            {...register("lastName")}
+            errorMessage={formState.errors.lastName?.message}
+          />
+        </div>
 
-      <div>
-        <label htmlFor="email">Email Address</label>
-        <InputText
-          id="email"
-          type="email"
-          containerClass="mt-2"
-          {...register("email")}
-          errorMessage={formState.errors.email?.message}
-        />
-      </div>
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <InputText
+            id="email"
+            type="email"
+            containerClass="mt-2"
+            {...register("email")}
+            errorMessage={formState.errors.email?.message}
+          />
+        </div>
 
-      <div>
-        <label htmlFor="phone">Phone Number</label>
-        <InputText
-          id="phone"
-          type="tel"
-          containerClass="mt-2"
-          {...register("phone")}
-          errorMessage={formState.errors.phone?.message}
-        />
-      </div>
+        <div>
+          <label htmlFor="phone">Phone Number</label>
+          <InputText
+            id="phone"
+            type="tel"
+            containerClass="mt-2"
+            {...register("phone")}
+            errorMessage={formState.errors.phone?.message}
+          />
+        </div>
 
-      <DevTool control={control} />
-    </form>
+        {import.meta.env.DEV && <DevTool control={control} />}
+      </form>
+    </Fade>
   );
 }
 
