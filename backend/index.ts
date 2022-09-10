3649204f8c2d6c960865c0dd67fe5b2794d3d1d7
mramirid <amir.muh.hakim@gmail.com>
2022-09-10 07:16:57 +0700

    feat(backend): add ejs

--- backend/index.ts
+++ backend/index.ts
@@ -2,8 +2,15 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.get("/", (_, res) => {
-  res.send("Hello, World!");
+  res.render("index", {
+    name: "Amir",
+    age: 99,
+    hobbies: ["Workout", "Coding", "Cleaning"],
+  });
 });
 
 app.listen(3000, () => console.log("Listening at http://localhost:3000"));
