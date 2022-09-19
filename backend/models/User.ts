b0c24391ed967428a6e3a7fea06c8cc2a79dc165
mramirid <amir.muh.hakim@gmail.com>
2022-09-19 15:58:12 +0700

    feat(backend): add user schema

---
+++ backend/models/User.ts
@@ -0,0 +1,28 @@
+import bcrypt from "bcryptjs";
+import { model, Schema } from "mongoose";
+
+export interface IUser {
+  username: string;
+  password: string;
+}
+
+const userSchema = new Schema<IUser>({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+  next();
+});
+
+const User = model("User", userSchema);
+export default User;
