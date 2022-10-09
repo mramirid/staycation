67353dd2b807c92e31cfd79fd9860699ea5297c2
mramirid <amir.muh.hakim@gmail.com>
2022-10-09 16:02:51 +0700

    refactor(client): simplify type overriding

--- client/src/types/react-router.ts
+++ client/src/types/react-router.ts
@@ -1,9 +1,4 @@
-import type {
-  Location,
-  NavigateOptions,
-  RelativeRoutingType,
-  To,
-} from "react-router-dom";
+import type { Location, NavigateOptions, To } from "react-router-dom";
 
 export interface StatefulLocation<State> extends Omit<Location, "state"> {
   state: State | null;
@@ -13,9 +8,7 @@ export interface StatefulNavigate<State> {
   (to: To, options: StatefulNavigateOptions<State>): void;
 }
 
-interface StatefulNavigateOptions<State> extends NavigateOptions {
-  replace?: boolean;
+interface StatefulNavigateOptions<State>
+  extends Omit<NavigateOptions, "state"> {
   state: State;
-  preventScrollReset?: boolean;
-  relative?: RelativeRoutingType;
 }
